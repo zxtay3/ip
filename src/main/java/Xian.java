e8c51b3ecb962f6c846377e9867a52958089bfa2
zx <zhongxiantay@gmail.com>
2026-08-19 13:38:21 +0800

    Added echo function

--- src/main/java/Xian.java
+++ src/main/java/Xian.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Xian {
     public static void main(String[] args) {
         String banner = " __  __ ___    _    _   _ \n"
@@ -9,6 +11,20 @@ public class Xian {
         System.out.println(banner);
         System.out.println("Hello, I'm " + botName + "!");
         System.out.println("What can I do for you today?\n");
-        System.out.println("Bye!! See you again soon!\n");
+
+        Scanner scanner = new Scanner(System.in);
+
+        while(true){
+            String input = scanner.nextLine();
+            System.out.println();
+
+            if (input.equals("bye")){
+                break;
+            }
+
+            System.out.println("\t" + input + "\n");
+        }
+
+        System.out.println("\tBye!! See you again soon!\n");
     }
 }

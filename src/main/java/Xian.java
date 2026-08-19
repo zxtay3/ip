e4952d88e5391fc659db371b3f5e492bb3d53009
zx <zhongxiantay@gmail.com>
2026-08-19 13:19:21 +0800

    Renamed filename and added greeting

--- src/main/java/Xian.java
+++ src/main/java/Xian.java
@@ -1,10 +1,14 @@
-public class Duke {
+public class Xian {
     public static void main(String[] args) {
         String banner = " __  __ ___    _    _   _ \n"
                 + " \\ \\/ /|_ _|  / \\  | \\ | |\n"
                 + "  \\  /  | |  / _ \\ |  \\| |\n"
                 + "  /  \\  | | / ___ \\| |\\  |\n"
                 + " /_/\\_\\|___/_/   \\_\\_| \\_|\n";
+        String botName = "XIAN";
         System.out.println(banner);
+        System.out.println("Hello, I'm " + botName + "!");
+        System.out.println("What can I do for you today?\n");
+        System.out.println("Bye!! See you again soon!\n");
     }
 }

f6739afcd6706c950e6272ddb30ef12144bdd942
zx <zhongxiantay@gmail.com>
2026-08-19 16:24:59 +0800

    Added the add to list functionality

--- src/main/java/Xian.java
+++ src/main/java/Xian.java
@@ -1,4 +1,7 @@
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Xian {
     public static void main(String[] args) {
@@ -8,6 +11,8 @@ public class Xian {
                 + "  /  \\  | | / ___ \\| |\\  |\n"
                 + " /_/\\_\\|___/_/   \\_\\_| \\_|\n";
         String botName = "XIAN";
+        List<String> tasks = new ArrayList<>();
+
         System.out.println(banner);
         System.out.println("Hello, I'm " + botName + "!");
         System.out.println("What can I do for you today?\n");
@@ -22,7 +27,15 @@ public class Xian {
                 break;
             }
 
-            System.out.println("\t" + input + "\n");
+            if(input.equals("list")){
+                for (int i = 0; i < tasks.size(); i++){
+                    System.out.println("\t" + (i+1) + ". " + tasks.get(i));
+                }
+                System.out.println();
+            }else {
+                tasks.add(input);
+                System.out.println("\t" + "added: " + input + "\n");
+            }
         }
 
         System.out.println("\tBye!! See you again soon!\n");

468793785ab396a9f7c919968b2282a0dd96593e
zx <zhongxiantay@gmail.com>
2026-08-23 23:00:14 +0800

    Add functionality of date and time format

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,23 +1,27 @@
-public class Event extends Task{
-    protected String from;
-    protected String to;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-    public Event(String description, String from, String to){
+public class Event extends Task{
+    protected LocalDateTime from;
+    protected LocalDateTime to;
+    private static final DateTimeFormatter DATE_TIME_FORMAT = DateTimeFormatter.ofPattern("d/M/yyyy HHmm");
+
+    public Event(String description, LocalDateTime from, LocalDateTime to){
         super(description);
         this.from = from;
         this.to = to;
     }
 
-    public String getFrom(){
+    public LocalDateTime getFrom(){
         return this.from;
     }
 
-    public String getTo(){
+    public LocalDateTime getTo(){
         return this.to;
     }
 
     @Override
     public String toString(){
-        return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
+        return "[E]" + super.toString() + " (from: " + from.format(DATE_TIME_FORMAT) + " to: " + to.format(DATE_TIME_FORMAT) + ")";
     }
 }

333fc6f345ac2e59cb4fda169001a4b8335846cb
zx <zhongxiantay@gmail.com>
2026-08-25 00:33:55 +0800

    Add parser logic and update main to work with it

---
+++ src/main/java/Parser.java
@@ -0,0 +1,53 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Parser{
+
+    private static final DateTimeFormatter DATE_TIME_FORMAT = DateTimeFormatter.ofPattern("d/M/yyyy HHmm");
+
+    public static String getCommandWord(String input){
+        return input.split(" ", 2)[0];
+    }
+
+    public static String getArguments(String input) throws XianException{
+        String[] parts = input.split(" ", 2);
+
+        if (parts.length < 2 || parts[1].isBlank()){
+            throw new XianException("Please remember to state task!! >:(");
+        }
+
+        return parts[1];
+    }
+
+    public static int parseIndex(String remainder) throws NumberFormatException{
+        return Integer.parseInt(remainder);
+    }
+
+    public static Task parseTodo(String remainder){
+        return new Todo(remainder);
+    }
+
+    public static Task parseDeadline(String remainder) throws XianException{
+        String[] taskDate = remainder.split(" /by ");
+
+        if (taskDate.length != 2 || taskDate[0].isBlank() || taskDate[1].isBlank()){
+            throw new XianException("Please ensure the format of the task is correct!! >:(");
+        }
+
+        LocalDateTime by = LocalDateTime.parse(taskDate[1], DATE_TIME_FORMAT);
+        return new Deadline(taskDate[0], by);
+    }
+
+    public static Task parseEvent(String remainder) throws XianException{
+        String[] taskDate = remainder.split(" /from | /to ");
+
+        if (taskDate.length != 3 || taskDate[0].isBlank() || taskDate[1].isBlank() || taskDate[2].isBlank()){
+            throw new XianException("Please ensure the format of the task is correct!! >:(");
+        }
+
+        LocalDateTime from = LocalDateTime.parse(taskDate[1], DATE_TIME_FORMAT);
+        LocalDateTime to = LocalDateTime.parse(taskDate[2], DATE_TIME_FORMAT);
+
+        return new Event(taskDate[0], from, to);
+    }
+}
--- src/main/java/Xian.java
+++ src/main/java/Xian.java
@@ -1,11 +1,7 @@
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.io.IOException;
 import java.time.format.DateTimeParseException;
 
 public class Xian {
-
-    private static final DateTimeFormatter DATE_TIME_FORMAT = DateTimeFormatter.ofPattern("d/M/yyyy HHmm");
 
     public static void main(String[] args) {
 
@@ -34,17 +30,11 @@ public class Xian {
                 }else if(input.equals("list")){
                     ui.showTaskList(tasks);
                 }else{
-                    String[] parts = input.split(" ", 2);
-                    String command = parts[0];
-
-                    if (parts.length < 2 || parts[1].isBlank()){
-                        throw new XianException("Please remember to state task!! >:(");
-                    }
-
-                    String remainder = parts[1];
+                    String command = Parser.getCommandWord(input);
+                    String remainder = Parser.getArguments(input);
 
                     if(command.equals("mark")){
-                        int idx = Integer.parseInt(remainder);
+                        int idx = Parser.parseIndex(remainder);
 
                         if(idx < 1 || idx > tasks.getSize()){
                             throw new XianException("Hello?! Please enter a valid item to mark  >:(");
@@ -56,7 +46,7 @@ public class Xian {
 
                         ui.showTaskMark(t);
                     }else if(command.equals("unmark")){
-                        int idx = Integer.parseInt(remainder);
+                        int idx = Parser.parseIndex(remainder);
 
                         if(idx < 1 || idx > tasks.getSize()){
                             throw new XianException("Hello?! Please enter a valid item to unmark  >:(");
@@ -68,7 +58,7 @@ public class Xian {
 
                         ui.showTaskUnmark(t);
                     }else if(command.equals("delete")){
-                        int idx = Integer.parseInt(remainder);
+                        int idx = Parser.parseIndex(remainder);
 
                         if(idx < 1 || idx > tasks.getSize()){
                             throw new XianException("Hello?! Please enter a valid item to delete  >:(");
@@ -82,31 +72,9 @@ public class Xian {
                         Task t;
 
                         switch (command) {
-                            case "todo" -> t = new Todo(remainder);
-                            case "deadline" -> {
-                                String[] task_date = remainder.split(" /by ");
-
-                                if (task_date.length != 2 || task_date[0].isBlank() || task_date[1].isBlank()){
-                                    throw new XianException("Please ensure the format of the task is correct!! >:(");
-                                }
-
-                                String task = task_date[0];
-                                LocalDateTime by = LocalDateTime.parse(task_date[1], DATE_TIME_FORMAT);
-                                t = new Deadline(task, by);
-                            }
-                            case "event" -> {
-                                String[] task_date = remainder.split(" /from | /to ");
-
-                                if (task_date.length != 3 || task_date[0].isBlank() || task_date[1].isBlank() || task_date[2].isBlank()){
-                                    throw new XianException("Please ensure the format of the task is correct!! >:(");
-                                }
-
-                                String task = task_date[0];
-                                LocalDateTime from = LocalDateTime.parse(task_date[1], DATE_TIME_FORMAT);
-                                LocalDateTime to = LocalDateTime.parse(task_date[2], DATE_TIME_FORMAT);
-
-                                t = new Event(task, from, to);
-                            }
+                            case "todo" -> t = Parser.parseTodo(remainder);
+                            case "deadline" -> t = Parser.parseDeadline(remainder);
+                            case "event" -> t = Parser.parseEvent(remainder);
                             default -> throw new XianException("Please enter a valid action :( ");
                         }
                         tasks.add(t);
